38af52cc36999e78cbde4c1bbbc3952bf471410f
K W <kw577@wp.pl>
2018-09-14 10:05:28 +0200

    Podstawowy szablon gry - okno gry, petla gry, metody tick() i render() odswiezajace stan gry

---
+++ Owls game/src/com/kw/owls/window/Game.java
@@ -0,0 +1,101 @@
+package com.kw.owls.window;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+
+	
+	/**
+	 *  Wygenerowany numer seryjny (nieobowiazkowe)
+	 */
+	private static final long serialVersionUID = 504219620878017710L;
+
+	private boolean running = false;
+	private Thread thread;
+	
+	
+	public synchronized void start() {
+		if(running)
+			return;
+		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	// petla gry !!!! - odswiezanie ekranu
+	// funkcja run() jest w wiekszosci przypadkow zbudowana w taki sposob i mozna ja po prostu skopiowac
+	public void run() {
+		this.requestFocus(); // dzieki temu po wlaczeniu gry klawiatura okno gry jest aktywne od razu - nie trzeba w nie najpierw kliknac aby moc sterowac gra
+		long lastTime = System.nanoTime(); // Returns the current value of the running Java Virtual Machine's high-resolution time source, in nanoseconds. 
+		double amountOfTicks = 60.0; // zmieniajac ta wartosc mozna spowodowac opoznienie lub przyspieszenie animacji
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis(); // Returns the current value of the running Java Virtual Machine's high-resolution time source, in nanoseconds.
+		int updates = 0;
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick(); // update ekranu gry (60 / s) 
+				updates++;
+				delta--;
+						
+			}
+			if(running)
+				render();
+				frames++;
+					
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				updates = 0;
+			}
+		}
+		//stop();
+		}
+	
+	
+
+	// updatowanie danych gry
+	private void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// updatowanie widoku ekranu
+	private void render() {
+		
+		BufferStrategy bs = this.getBufferStrategy(); // refers to Canvas class - represents the mechanism with which to organize complex memory on a particular Canvas or Window
+		if(bs == null) {
+			this.createBufferStrategy(3); // create a BufferStrategy with that number of buffers
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		/////////////////////////////
+		// renderowanie grafiki gry
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		
+		/////////////////////////////
+		g.dispose();
+		bs.show();
+		
+	}
+	
+	public static void main(String args[]) {
+		new Window(800, 600, "Building game", new Game());
+	}
+	
+}
---
+++ Owls game/src/com/kw/owls/window/Window.java
@@ -0,0 +1,30 @@
+package com.kw.owls.window;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window {
+
+	// konstruktor
+	public Window(int w, int h, String title, Game game) {
+		
+		game.setPreferredSize(new Dimension(w, h));
+		game.setMaximumSize(new Dimension(w, h));
+		game.setMinimumSize(new Dimension(w, h));
+		
+		JFrame frame = new JFrame(title);
+		frame.add(game);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		game.start();
+	}
+	
+	
+	
+	
+}
