65a99693de233bb8bee5adec7d7afe65094833a7
K W <kw577@wp.pl>
2018-09-27 22:45:07 +0200

    Dodano grafike do HUD - commit nie zawiera folderu z grafikami

--- Owls game/src/com/kw/owls/window/HUD.java
+++ Owls game/src/com/kw/owls/window/HUD.java
@@ -3,54 +3,75 @@ package com.kw.owls.window;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import com.kw.owls.framework.SpriteSheet;
 
 // klasa monitoruje i wyswietla stan parametrow gry - zdrowie gracza, ilosc punktow i.t.d
 
 public class HUD {
 
 	private Game game;
-
+	private BufferedImage player_av = null;
+	private BufferedImage flower_av = null;
+	private SpriteSheet numbersSpriteSheet = null;
+	private BufferedImage numbers = null;
+	
+	// Pomocniczo do generowania grafiki przedstawiajacej stan punktacji
+	private int score_copy = 0;
+	private int pom = 0;
+	private int counter = 0; // zlicza ile cyfr ma liczba reprezentujaca ilosc punktow uzyskane przez gracza
+	
 	// Konstruktor
 	public HUD(Game game) {
 		
 		this.game = game;
+		
+		// zaladowanie grafiki
+		BufferedImageLoader loader = new BufferedImageLoader();
+		try {
+			// siatka col x row = 44 x 50 px dla pliku numbers_spritesheet.png oraz 35 x 40 px dla pliku numbers_smaller_spritesheet.png
+			numbers = loader.loadImage("/hud/numbers_smaller_spritesheet.png"); 
+			player_av = loader.loadImage("/hud/player_avatar.png");
+			flower_av = loader.loadImage("/hud/flower_avatar.png");
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		numbersSpriteSheet = new SpriteSheet(numbers);
+		
 	}
 	
 	
 	public void render(Graphics g) {
-		g.setColor(Color.blue);
 		
-		Font font = new Font("Jokerman",Font.BOLD, 25);
-		g.setFont(font);
+		//// wyswietlanie stanu zdrowia gracza w tym bloku
+		g.drawImage(player_av, 0, 0, null);
+		g.drawImage(numbersSpriteSheet.grabImage(3, 2, 35, 40), 60, 12, null); // znak "x" 
 		
-		 int ter = 123;
+		if(game.getHealth() >= 0)
+		g.drawImage(numbersSpriteSheet.grabImage(game.getHealth() + 1, 1, 35, 40), 100, 10, null); // wyswietla obrazek odpowiadajacy ilosci punktow zdrowia
 		
-		g.drawString("Health: x" + game.getHealth(), 35, 40);
-		g.drawString("Score: x" + game.getFlowers(), 35, 80);
-		System.out.println("\n Obliczenia: ");
-		int x = 110;
-		int y = 40;
-		int temp = game.getHealth();
-		int counter = 0;
+		////
 		
-		/*
-		while(temp > 0) {
-			temp--;
-			g.drawImage(health_image, x, y, null);
+		
+		// wyswietla ilosc zdobytych punktow 
+		g.drawImage(flower_av, 725, 0, null);
+		
+		score_copy = game.getFlowers();
+		while(score_copy > 0) {
+			pom = score_copy % 10;
+			g.drawImage(numbersSpriteSheet.grabImage(pom + 1, 1, 35, 40), 695 - counter*35, 10, null); // wyswietla obrazek odpowiadajacy ilosci punktow zdrowia
+				
+			score_copy = (int)(score_copy - pom)/10;
 			counter++;
-			x += 24;
-			if (counter >= 5) {
-				counter = 0;
-				y += 24;
-				x = 110;
-			}
-			
 		}
-		*/
+		counter = 0;
+	
+		//
+		
 		
 	}
-	
-	
 	
 	
 }
